682c431fa16579819898bb610f790ac1bb8667fc
gdhughes5 <gdhughes5@hotmail.com>
2020-08-27 01:21:37 -0400

    Initial Commit

---
+++ Teapot/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Teapot
+//
+//  Created by Gabe Hughes on 8/27/20.
+//  Copyright © 2020 Gabe Hughes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
